5da429ac574baa334ba5f529a8e6f54b56569cb4
Issac In <66139562+issac-in@users.noreply.github.com>
2024-05-07 16:44:36 -0700

    Abstract PEMDAS logic to operate()
    Before, you'd have to call the specific functions to do any PEMDAS sort
    of calculations, and now you only have to call operate, and it'll pick
    up based on the operator what you want to do.
    
    Reordered the function calls in to the operate() being at top of file,
    and having the prior alphabetic ordering of functions to PEMDAS order.
    
    Any other functions that may be necessary in the future will be in
    alphabetical order below the operate and PEMDAS functions.

--- calculator.js
+++ calculator.js
@@ -1,15 +1,50 @@
 "use strict";
 
-function add(operand1, operand2) {
-    return operand1 + operand2;
+let operand1;
+let operand2;
+let operator;
+
+function operate(operand1, operand2, operator) {
+    let operation;
+    switch (operator) {
+        case "**":
+        case "^":
+            operation = exponent(operand1, operand2);
+            break;
+        case "*":
+            operation = multiply(operand1, operand2);
+            break;
+        case "/":
+            operation = divide(operand1, operand2);
+            break;
+        case "+":
+            operation = add(operand1, operand2);
+            break;
+        case "-":
+            operation = subtract(operand1, operand2);
+            break;
+    }
+    return operation;
+}
+
+/*
+    P.E.M.D.A.S. functions
+*/
+
+function exponent(base, power) {
+    return base ** power;
+}
+
+function multiply(operand1, operand2) {
+    return operand1 * operand2;
 }
 
 function divide(operand1, operand2) {
     return operand1 / operand2;
 }
 
-function multiply(operand1, operand2) {
-    return operand1 * operand2;
+function add(operand1, operand2) {
+    return operand1 + operand2;
 }
 
 function subtract(operand1, operand2) {
