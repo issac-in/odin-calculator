72ee7a3edf8c6bb36a256d7201a45fb491d37236
Issac In <66139562+issac-in@users.noreply.github.com>
2024-05-07 17:52:03 -0700

    Restrict PEMDAS core to 10 decimal places
    Input numbers can be whatever, the output number will be restricted to
    10 decimal places for the sake of TDD.
    
    Why: The calculator I am using to validate the test cases with manually
    as I initially create them restricts the decimal places of results to
    10 decimal places.

--- calculator.js
+++ calculator.js
@@ -28,27 +28,28 @@ function operate(operand1, operand2, operator) {
 }
 
 /*
-    P.E.M.D.A.S. functions
+    PEMDAS core functions
+    - Calculated numbers restricted to 10 decimal places for sake of TDD.
 */
 
 function exponent(base, power) {
-    return base ** power;
+    return +((base ** power).toFixed(10));
 }
 
 function multiply(operand1, operand2) {
-    return operand1 * operand2;
+    return +((operand1 * operand2).toFixed(10));
 }
 
 function divide(operand1, operand2) {
-    return operand1 / operand2;
+    return +((operand1 / operand2).toFixed(10));
 }
 
 function add(operand1, operand2) {
-    return operand1 + operand2;
+    return +((operand1 + operand2).toFixed(10));
 }
 
 function subtract(operand1, operand2) {
-    return operand1 - operand2;
+    return +((operand1 - operand2).toFixed(10));
 }
 
 // Why the if(): https://bit.ly/fix_for_referenceError_module_is_not_defined
